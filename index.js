72f365f37efa9eef8057dca64e2e4770bf613cf2
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-10-02 06:29:18 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your Circle class here
+
+class Circle {
+    constructor(radius) {
+      this._radius = radius;
+    }
+  
+    // Getter for radius
+    get radius() {
+      return this._radius;
+    }
+  
+    // Getter for diameter (pseudo-property)
+    get diameter() {
+      return this._radius * 2;
+    }
+  
+    // Getter for circumference (pseudo-property)
+    get circumference() {
+      return 2 * Math.PI * this._radius;
+    }
+  
+    // Getter for area (pseudo-property)
+    get area() {
+      return Math.PI * Math.pow(this._radius, 2);
+    }
+  
+    // Setter for diameter (pseudo-property)
+    set diameter(value) {
+      if (value >= 0) {
+        this._radius = value / 2;
+      }
+    }
+  
+    // Setter for circumference (pseudo-property)
+    set circumference(value) {
+      if (value >= 0) {
+        this._radius = value / (2 * Math.PI);
+      }
+    }
+  
+    // Setter for area (pseudo-property)
+    set area(value) {
+      if (value >= 0) {
+        this._radius = Math.sqrt(value / Math.PI);
+      }
+    }
+  }
+  
+  module.exports = Circle;
+  
